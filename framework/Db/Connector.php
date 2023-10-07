11a6df5f4d5da3dc5c645dbb4f68ba29800296fe
Radagea <radagea@gmail.com>
2023-10-07 15:00:33 +0200

    Added models save function
    abstract model class:
    Added save function the function call the connector class executeI or executeU function
    
    Connector class
    Added executeU and executeI for update and insert
    Created buildWhere seperate function its build where for the SQL SELECT or UPDATE functions

--- framework/Db/Connector.php
+++ framework/Db/Connector.php
@@ -36,9 +36,58 @@ class Connector
         return self::$instance;
     }
 
-    public function fetchQuery($query) : Array
+    public function fetchQuery(String $query) : Array
     {
         return $this->conn->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function executeI(String $table, Array $fields) : bool|int
+    {
+        $sql = 'INSERT INTO ' . $table . ' ';
+        $field_names = '(';
+        $fields_value = '(';
+
+        foreach ($fields as $key => $field) {
+            if ($key != array_key_last($fields)) {
+                $field_names .= $key . ', ';
+                $fields_value .= ':' . $key . ', ';
+            } else {
+                $field_names .= $key . ')';
+                $fields_value .= ':' . $key . ')';
+            }
+        }
+
+        $sql .= $field_names . ' VALUES ' . $fields_value;
+
+        $return = $this->conn->prepare($sql)->execute($fields);
+
+        if ($return) {
+            return $this->conn->lastInsertId();
+        }
+
+        return false;
+    }
+    public function executeU(String $table, Array $fields, Array $wheres = null) : bool
+    {
+        $sql = 'UPDATE ' . $table. ' SET';
+
+        foreach ($fields as $key => $field) {
+            $sql .= ' ' . $key . '=:' . $key;
+
+            if ($key != array_key_last($fields)) {
+                $sql .= ', ';
+            } else {
+                $sql .= ' ';
+            }
+        }
+
+        if ($wheres != null) {
+            $sql .= $this->buildWhere($wheres);
+        }
+
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute($fields);
     }
 
     public function buildModelSelect(String $table, Array $wheres = null, Array $orderBy = null, Array|int $limit = null) : String
@@ -46,17 +95,7 @@ class Connector
         $query = 'SELECT * FROM ' . $table;
 
         if ($wheres != null) {
-            foreach ($wheres as $key => $where) {
-                if ($key != 0 && !array_key_exists('prev_rel', $where)) {
-                    throw new \Exception('The prev_rel (previous relationship) operator missing!');
-                }
-
-                if ($key == 0) {
-                    $where['prev_rel'] = 'WHERE';
-                }
-
-                $query .= ' ' . $where['prev_rel'] . ' ' . $where['field'] . $where['operator'] . $where['value'];
-            }
+            $query .= $this->buildWhere($wheres);
         }
 
         if ($orderBy != null) {
@@ -73,6 +112,44 @@ class Connector
             } else {
                 $query .= ' LIMIT ' . $limit;
             }
+        }
+
+        return $query;
+    }
+
+    /**
+     * Dev comment:
+     * $wheres = [
+     *      [
+     *          'field'    => 'username',
+     *          'operator' => '=',
+     *          'value'    => 'MajFrame'
+     *      ],
+     *      [
+     *          'field'    => 'email'
+     *          'operator' => '='
+     *          'value'    => 'majframe@majframe.com'
+     *          'prev_rel' => 'AND'
+     *      ]
+     * ]
+     */
+    private function buildWhere(Array $wheres) : String
+    {
+        $query = '';
+        foreach ($wheres as $key => $where) {
+            if ($key != 0 && !array_key_exists('prev_rel', $where)) {
+                throw new \Exception('The prev_rel (previous relationship) operator missing!');
+            }
+
+            if (!array_key_exists('operator', $where)) {
+                $where['operator'] = '=';
+            }
+
+            if ($key == 0) {
+                $where['prev_rel'] = 'WHERE';
+            }
+
+            $query .= ' ' . $where['prev_rel'] . ' ' . $where['field'] . $where['operator'] . $where['value'];
         }
 
         return $query;
